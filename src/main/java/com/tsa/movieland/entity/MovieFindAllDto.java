b0da3218e064c2806ed78ef7fbaa01748982ee90
Serhii Trehubenko <tsa85.ca@gmail.com>
2023-08-15 12:43:47 +0300

    [b-10] add movie part 1.1

---
+++ src/main/java/com/tsa/movieland/entity/MovieFindAllDto.java
@@ -0,0 +1,17 @@
+package com.tsa.movieland.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+@Builder
+@Getter
+public class MovieFindAllDto {
+    private int id;
+    private String nameRussian;
+    private String nameNative;
+    private int yearOfRelease;
+    private double rating;
+    private double price;
+    private List<String> picturePath;
+}
